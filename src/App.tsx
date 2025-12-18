cf5a4a1611875d2c10d080220ebbae11c0623717
DavidUdeji <davidcudeji@gmail.com>
2025-12-18 01:33:32 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,190 @@
+import { useEffect, useState, useEffectEvent, useRef } from "react";
+import "./App.css";
+import { shuntingYardAlgorithm } from "./algo";
+
+function App() {
+  const [input, setInput] = useState<string>("0");
+  const [isDelClicked, setIsDelClicked] = useState(false);
+  const isWaitingForOperand = useRef<boolean>(false);
+
+  const onInputEmpty = useEffectEvent(() => {
+    setInput("0");
+  });
+
+  useEffect(() => {
+    if (isDelClicked && input.length === 0) {
+      onInputEmpty();
+    }
+  }, [input, isDelClicked]);
+
+  function handleNumClick(value: string) {
+    if (input === "0") {
+      setInput(() => value);
+      return;
+    }
+
+    setInput((oldInput) => oldInput + value);
+    isWaitingForOperand.current = false;
+  }
+
+  function handleClearClick() {
+    setInput("0");
+  }
+
+  function handleDelClick() {
+    setIsDelClicked(true);
+    setInput((oldInput) => {
+      return oldInput.slice(0, oldInput.length - 1);
+    });
+  }
+
+  function handleDotClick() {
+    setInput((oldInput) => oldInput + ".");
+  }
+
+  function handleOperator(operator: "+" | "-" | "*" | "/") {
+    if (input === "0") return;
+
+    isWaitingForOperand.current = true;
+
+    setInput((oldInput) => oldInput + operator);
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (!isWaitingForOperand || !input[input.length - 1].match(/[0-9]/)) return;
+
+    const inputArray = convertStrToArray(input);
+    const answer = shuntingYardAlgorithm(inputArray);
+
+    const isDecimal = String(answer).indexOf(".") !== -1;
+    const shouldFixToTen = isDecimal && String(answer).length > 10;
+    const finalAnswer = shouldFixToTen
+      ? String(answer.toFixed(10))
+      : String(answer);
+
+    setInput(() => finalAnswer);
+  }
+
+  function convertStrToArray(input: string) {
+    let numSequence = "";
+    const result = [];
+    for (let i = 0; i < input.length; i++) {
+      const char = input[i];
+      const isNumber = !Number.isNaN(Number(char));
+      if (isNumber || char === ".") {
+        numSequence += char;
+
+        if (i == input.length - 1) {
+          result.push(numSequence);
+          numSequence = "";
+        }
+      } else {
+        result.push(numSequence);
+        result.push(char);
+        numSequence = "";
+      }
+    }
+
+    return result;
+  }
+
+  return (
+    <form
+      style={{
+        width: "100",
+        maxWidth: "200px",
+        margin: "0 auto",
+        display: "grid",
+      }}
+      onSubmit={handleSubmit}
+    >
+      <input
+        type="text"
+        readOnly
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+        style={{
+          textAlign: "right",
+          padding: "1rem 0",
+          fontSize: "1.5rem",
+        }}
+      />
+
+      <div
+        id="keys"
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(4, 1fr)",
+        }}
+      >
+        <button type="button" id="delete" onClick={handleDelClick}>
+          X
+        </button>
+        <button type="button" id="clear" onClick={handleClearClick}>
+          AC
+        </button>
+        <button type="button" id="percent">
+          %
+        </button>
+        <button type="button" id="divide" onClick={() => handleOperator("/")}>
+          /
+        </button>
+
+        <button type="button" id="7" onClick={() => handleNumClick("7")}>
+          7
+        </button>
+        <button type="button" id="8" onClick={() => handleNumClick("8")}>
+          8
+        </button>
+        <button type="button" id="9" onClick={() => handleNumClick("9")}>
+          9
+        </button>
+        <button type="button" id="multiply" onClick={() => handleOperator("*")}>
+          X
+        </button>
+
+        <button type="button" id="4" onClick={() => handleNumClick("4")}>
+          4
+        </button>
+        <button type="button" id="5" onClick={() => handleNumClick("5")}>
+          5
+        </button>
+        <button type="button" id="6" onClick={() => handleNumClick("6")}>
+          6
+        </button>
+        <button type="button" id="subtract" onClick={() => handleOperator("-")}>
+          -
+        </button>
+
+        <button type="button" id="1" onClick={() => handleNumClick("1")}>
+          1
+        </button>
+        <button type="button" id="2" onClick={() => handleNumClick("2")}>
+          2
+        </button>
+        <button type="button" id="3" onClick={() => handleNumClick("3")}>
+          3
+        </button>
+        <button type="button" id="addition" onClick={() => handleOperator("+")}>
+          +
+        </button>
+
+        <button type="button" id="inverse">
+          +/-
+        </button>
+        <button type="button" id="0" onClick={() => handleNumClick("0")}>
+          0
+        </button>
+        <button type="button" id="dot" onClick={handleDotClick}>
+          .
+        </button>
+        <button type="submit" id="equals">
+          =
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default App;
---
+++ src/algo.ts
@@ -0,0 +1,73 @@
+const stack: string[] = [];
+let queue: (number | string)[] = [];
+let numStack: number[] = [];
+const operatorPrecedence = {
+  "+": 1,
+  "-": 1,
+  "*": 2,
+  "/": 2,
+};
+
+export function shuntingYardAlgorithm(input: string[]) {
+  for (let i = 0; i < input.length; i++) {
+    const char = input[i];
+    const isNumber = !Number.isNaN(Number(char));
+
+    if (isNumber) {
+      queue.push(Number(char));
+    } else {
+      while (
+        stack.length &&
+        operatorPrecedence[stack.at(-1)!] >= operatorPrecedence[char]
+      ) {
+        queue.push(stack.pop()!);
+      }
+
+      stack.push(char);
+    }
+  }
+
+  while (stack.length) {
+    queue.push(stack.pop()!);
+  }
+
+  // Part: doing the actual calculation
+  for (let i = 0; i < queue.length; i++) {
+    const char = queue[i];
+
+    const isNumber = !Number.isNaN(Number(char));
+
+    if (isNumber) {
+      numStack.push(char);
+    } else {
+      const right = numStack.pop()!; // 5
+      const left = numStack.pop()!; // 4
+
+      if (left && right) {
+        if (char === "+") {
+          const result = add(left, right);
+          numStack.push(result);
+        } else if (char === "-") {
+          const result = left - right;
+          numStack.push(result);
+        } else if (char === "/") {
+          const result = left / right;
+          numStack.push(result);
+        } else if (char === "*") {
+          const result = left * right;
+          numStack.push(result);
+        }
+      }
+    }
+  }
+
+  const result = numStack[0];
+  queue = [];
+  numStack = [];
+
+  return result;
+}
+
+function add(a: number, b: number): number {
+  return a + b;
+}
